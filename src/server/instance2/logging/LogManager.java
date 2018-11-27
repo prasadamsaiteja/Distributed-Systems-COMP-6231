34f8fc817d081c0805f2275d5b9cfa89e63f7655
saiteja <saitejaprasadam@gmail.com>
2018-11-27 17:23:14 -0500

    merge conflict fixed

--- src/server/instance2/logging/LogManager.java
+++ src/server/instance2/logging/LogManager.java
@@ -1,36 +1,9 @@
 package server.instance2.logging;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.logging.FileHandler;
-import java.util.logging.Handler;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
-
-/*public class LogManager {
-	public Handler fileHandler = null;
-	public Logger logger;
-	
-	public LogManager(String serverName) {
-        
-		logger = Logger.getLogger(serverName);
-		try {
-			
-			fileHandler = new FileHandler(serverName + ".log",true);//creating the server log with server name
-			SimpleFormatter formatter = new SimpleFormatter();
-	        fileHandler.setFormatter(formatter);
-	      //configuring the logger
-	        logger.setUseParentHandlers(false);
-			logger.addHandler(fileHandler);	
-			logger.setLevel(Level.INFO);	
-			
-		} catch (Exception e) {
-			logger.log(Level.SEVERE,"Exception in logger :: "+e.getMessage());
-		}
-	}
-}*/
-
 
 public class LogManager {
 	
